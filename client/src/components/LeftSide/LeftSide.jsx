41889f4c13eabffd60ac705a02389c54018b9d0c
rakibul-mahin <mahin.z.hasan137@gmail.com>
2023-03-17 18:46:39 +0600

    Designed the frontend part for the notification and explore section

--- client/src/components/LeftSide/LeftSide.jsx
+++ client/src/components/LeftSide/LeftSide.jsx
@@ -4,8 +4,126 @@ import "./leftSide.css";
 const LeftSide = () => {
   return (
     <div className='left-side'>
-      <div className='left-kichu-ekta'>
-        <p>kichu ekta</p>
+      <div className='container-one'>
+        <div className='left-content'>
+          <p>Notifications</p>
+          <p style={{ color: "#aaa" }}>See all</p>
+        </div>
+        <div className='left-user-detail'>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='left-img'
+          />
+          <p
+            style={{
+              marginLeft: "10px",
+              fontSize: 13,
+              textAlign: "start",
+              width: "120px",
+            }}
+          >
+            Test Likes your Post
+          </p>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='like-img'
+          />
+        </div>
+        <div className='left-user-detail'>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='left-img'
+          />
+          <p
+            style={{
+              marginLeft: "10px",
+              fontSize: 13,
+              textAlign: "start",
+              width: "120px",
+            }}
+          >
+            Test started following you
+          </p>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='follow-img'
+          />
+        </div>
+        <div className='left-user-detail'>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='left-img'
+          />
+          <p
+            style={{
+              marginLeft: "10px",
+              fontSize: 13,
+              textAlign: "start",
+              width: "120px",
+            }}
+          >
+            Test commented on your post
+          </p>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='like-img'
+          />
+        </div>
+      </div>
+
+      <div className='container-one'>
+        <div className='left-content'>
+          <p>Explore</p>
+          <p style={{ color: "#aaa" }}>See all</p>
+        </div>
+        <div>
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+          <img
+            src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnRvb258ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'
+            alt='profimg'
+            className='explore-img'
+          />
+        </div>
       </div>
     </div>
   );
